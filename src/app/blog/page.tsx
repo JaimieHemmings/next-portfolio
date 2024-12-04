4408b0772ee3731cb413036bef313d3f9396a430
Jaimie <jaimie.j.hemmings@gmail.com>
2024-12-04 16:49:17 +0000

    test

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -18,7 +18,7 @@ const page = async () => {
       </Heading>
       <div className="prose prose-xl prose-invert my-10">
         <p>
-          Welcome to my blog, here you&apos;ll find a collection of articles on web development, technology, and design. I hope you find them helpful and informative.
+          Welcome to my blog! Here you&apos;ll find a collection of articles on web development, technology, and design. I hope you find them helpful and informative.
         </p>
       </div>
       <ContentList posts={blogPosts} />
