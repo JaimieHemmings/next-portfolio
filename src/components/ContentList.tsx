f8d19cd6f60dd56f9ec64cfb9905401daecfbcd2
Jaimie <jaimie.j.hemmings@gmail.com>
2024-12-04 10:50:59 +0000

    Update ContentList.tsx

--- src/components/ContentList.tsx
+++ src/components/ContentList.tsx
@@ -5,13 +5,34 @@ import React, { useEffect, useRef, useState } from 'react'
 import { MdArrowOutward } from 'react-icons/md'
 import { gsap } from 'gsap'
 
-type ContentListProps = {
-  items: {
-    title: string
-    description: string
-    image: string
-  }
-}
+type ContentItem = {
+  title: string;
+  tags: string[];
+  image: string;
+};
+
+const itemList: ContentItem[] = [
+  {
+    title: "Blog Post Title 1",
+    tags: ["JavaScript", "React"],
+    image: "/image/profile-image.jpg"
+  },
+  {
+    title: "Blog Post Title 2",
+    tags: ["JavaScript", "React"],
+    image: "/image/profile-image.jpg"
+  },
+  {
+    title: "Blog Post Title 3",
+    tags: ["JavaScript", "React"],
+    image: "/image/profile-image.jpg"
+  },
+  {
+    title: "Blog Post Title 1",
+    tags: ["CSS", "Tailwind"],
+    image: "/image/profile-image.jpg"
+  },
+];
 
 export default function ContentList() {
 
@@ -44,7 +65,8 @@ export default function ContentList() {
             y: gsap.utils.clamp(0, maxY, mousePos.y - 160),
             rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1),
             ease: "back.out(2)",
-            duration: 1.3
+            duration: 1.3,
+            opacity: 1,
           })
 
         }
@@ -62,10 +84,6 @@ export default function ContentList() {
     }
   }, [currentItem]);
 
-  const contentImages = [
-    "/image/profile-image.jpg"
-  ]
-
   const onMouseEnter = (index: number) => {
     setCurrentItem(index);
   }
@@ -80,34 +98,40 @@ export default function ContentList() {
         onMouseLeave={onMouseLeave}
         className="grid border-b border-b-slate-100"
       >
-        <li
-          key="1"
-          className="list-item opacity-0f"
-          onMouseEnter={() => onMouseEnter(0)}  
-        >
-          <Link
-            href="#"
-            className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
-            aria-label="Blog Post Title">
-            <div className="flex flex-col">
-              <span className="text-3xl font-bold">Blog Post Title</span>
-              <div className="flex gap-3 text-yellow-400 text-lg font-bold">
-                <span>JavaScript</span>
-                <span>React</span>
+        {itemList.map((item, index) => (
+          <li
+            key={index}
+            className="list-item opacity-0f"
+            onMouseEnter={() => onMouseEnter(1)}  
+          >
+            <Link
+              href="#"
+              className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
+              aria-label="Blog Post Title">
+              <div className="flex flex-col">
+                <span className="text-3xl font-bold">{item.title}</span>
+                <div className="flex gap-3 text-yellow-400 text-lg font-bold">
+                  {item.tags.map((tag, index) => (
+                    <span key={index}>{tag}</span>
+                  ))}
+                </div>
               </div>
-            </div>
-            <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
-              Read More <MdArrowOutward />
-            </span>
-          </Link>
-        </li>
+              <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
+                Read More <MdArrowOutward />
+              </span>
+            </Link>
+          </li>
+        ))}
       </ul>
 
       <div
-        ref={revealRef}
-        className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300" style={{backgroundImage: currentItem != null ? "image/profile-image.jpg" : "image/profile-image.jpg"}}>
-
-      </div>
+          className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300"
+          style={{
+            backgroundImage:
+              currentItem !== null ? `url(${itemList[currentItem].image})` : "",
+          }}
+          ref={revealRef}
+        ></div>
     </div>
   )
 }
