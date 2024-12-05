67ace28d3c52051fba46efef77d02b6b93f12008
Jaimie <jaimie.j.hemmings@gmail.com>
2024-12-05 21:57:52 +0000

    fix: typo

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
         Contact
       </Heading>
       <div className="prose prose-xl prose-slate prose-invert col-start-1">
-        <p>Please bare with me as I build out this page. In the meantime, feel free to reach out to me on <a href="https://www.linkedin.com/in/jaimiehemmings/" target="_blank" rel="noopener noreferrer" className="text-yellow-400">LinkedIn</a>.</p>
+        <p>Please bear with me as I build out this page. In the meantime, feel free to reach out to me on <a href="https://www.linkedin.com/in/jaimiehemmings/" target="_blank" rel="noopener noreferrer" className="text-yellow-400">LinkedIn</a>.</p>
       </div>
     </Bounded>
   )
